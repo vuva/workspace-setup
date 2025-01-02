dbd7890437b802762fb05b3e847ead51d5e4ae02
Vu Vu <4353779+vuva@users.noreply.github.com>
2025-01-02 13:40:54 +0100

    Update setup_lazyvim.sh
    Add installation of venv and pynvim
    Change lazy starter repo to personal repo

--- setup_lazyvim.sh
+++ setup_lazyvim.sh
@@ -1,7 +1,10 @@
 sudo apt update
-sudo apt install xclip xsel ripgrep fd-find npm
+sudo apt install xclip xsel ripgrep fd-find npm nodejs fzf git
 sudo snap refresh
 sudo snap install nvim --classic
+
+# Ubuntu 24
+sudo apt install python3-venv python3-pynvim 
 
 # required
 mv ~/.config/nvim{,.bak}
@@ -11,6 +14,6 @@ mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim/
+git clone https://github.com/vuva/lazyvim-starter.git ~/.config/nvim/
 
-rm -rf ~/.config/nvim/.git
+# rm -rf ~/.config/nvim/.git
